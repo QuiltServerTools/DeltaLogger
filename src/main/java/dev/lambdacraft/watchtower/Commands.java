a3b3014f1a6cc9cb0e2f298e4d39d62691a57cc7
unsupervised <unsupervised@tutanota.com>
2020-11-27 18:40:59 +0100

    Remove enderchest stuff

--- src/main/java/dev/lambdacraft/watchtower/Commands.java
+++ src/main/java/dev/lambdacraft/watchtower/Commands.java
@@ -1,25 +1,20 @@
 package dev.lambdacraft.watchtower;
 
-import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.collection.DefaultedList;
 
 import static net.minecraft.server.command.CommandManager.*;
-import static com.mojang.brigadier.arguments.StringArgumentType.word;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * WatchTower admin commands
@@ -33,8 +28,6 @@ public class Commands {
       literal("wt")
         .requires(source -> source.hasPermissionLevel(4))
         .then(literal("tool").executes(Commands::toggleTool))
-        .then(literal("takeitems")
-          .then(argument("player", word()).executes(Commands::takeItems)))
     );
   }
 
@@ -78,41 +71,4 @@ public class Commands {
        }
     }
   }
-
-  /**
-   * Command to duplicate items of player and spit them at the feet of the command
-   * source entity. Mainly intended foor getting banned player's inventory.
-   * @param ctx
-   * @return
-   * @throws CommandSyntaxException
-   */
-  private static int takeItems(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
-    try {
-      PlayerEntity caller = ctx.getSource().getPlayer();
-      MinecraftServer server = ctx.getSource().getMinecraftServer();
-      String toLookup = StringArgumentType.getString(ctx, "player");
-      UUID uuid = null;
-      caller.sendMessage(new LiteralText("this command is broken for now :("), true);
-      return 1;
-      // IPlayerLoader loader = (IPlayerLoader)(Object)server.getWorld(caller.dimension).getSaveHandler();
-      // try {
-      //   uuid = UUID.fromString(toLookup);
-      // } catch (IllegalArgumentException e) {}
-      // GameProfile profile = uuid == null
-      //   ? server.getUserCache().findByName(toLookup)
-      //   : server.getUserCache().getByUuid(uuid);
-      // if (profile == null) {
-      //   caller.sendMessage(new LiteralText("No player found for that name/id"), true);
-      //   return 1;
-      // }
-  
-      // List<DefaultedList<ItemStack>> inventory = loader.loadPlayerInventory(profile.getId());
-      // dropItemsAtPlayer(caller, inventory);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    return 1;
-  }
-
 }
