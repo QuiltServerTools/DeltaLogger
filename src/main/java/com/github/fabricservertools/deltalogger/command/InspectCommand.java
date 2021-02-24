d4972cf64049d5acea5767c8636c2d4ff7e4f168
yitzy299 <hugocarter@hotmail.co.uk>
2021-02-24 18:03:14 +0000

    Fix translations

--- src/main/java/com/github/fabricservertools/deltalogger/command/InspectCommand.java
+++ src/main/java/com/github/fabricservertools/deltalogger/command/InspectCommand.java
@@ -80,7 +80,7 @@ public class InspectCommand {
 		MutableText placementMessage = DAO.block.getLatestPlacementsAt(dimension, pos, 0, limit).stream()
 				.map(p -> p.getText()).reduce((p1, p2) -> Chat.concat("\n", p1, p2))
 				.map(txt -> Chat.concat("\n", new TranslatableText("deltalogger.history.placement"), txt))
-				.orElse(new TranslatableText("deltalogger.none_found.pos", "placements", pos.toShortString()));
+				.orElse(new TranslatableText("deltalogger.none_found.pos", "placements", pos.toString()));
 
 		// FIXME: fix getting block from server console
 		Chat.send(scs.getSource().getPlayer(), placementMessage);
