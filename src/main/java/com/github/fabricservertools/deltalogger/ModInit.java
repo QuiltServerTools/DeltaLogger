afa75813d5b92ef2a6a3dc2f191671d3e1c9f14a
konsolas <vincentyntang@gmail.com>
2021-02-27 10:39:15 +0000

    Replace PlayerEventsMixin with fabric-api ServerPlayConnectionEvents

--- src/main/java/com/github/fabricservertools/deltalogger/ModInit.java
+++ src/main/java/com/github/fabricservertools/deltalogger/ModInit.java
@@ -1,14 +1,18 @@
 package com.github.fabricservertools.deltalogger;
 
 import com.github.fabricservertools.deltalogger.command.Commands;
+import com.github.fabricservertools.deltalogger.dao.PlayerDAO;
 import com.github.fabricservertools.deltalogger.dao.RegistryDAO;
 import com.github.fabricservertools.deltalogger.gql.ApiServer;
 import com.google.common.collect.Sets;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
 import net.minecraft.util.registry.Registry;
@@ -49,7 +53,7 @@ public class ModInit implements ModInitializer {
 		return CONFIG;
 	}
 
-	public void onServerSetup(MinecraftServer server) {
+	private void onServerSetup(MinecraftServer server) {
 		dm = DatabaseManager.create(server.getSavePath(WorldSavePath.ROOT).toFile());
 
 		String portString = CONFIG.getProperty("webapp_port", "8080");
@@ -60,7 +64,7 @@ public class ModInit implements ModInitializer {
 		}
 	}
 
-	public void onServerStart(MinecraftServer server) {
+	private void onServerStart(MinecraftServer server) {
 		HashSet<Identifier> dimensionIds = new HashSet<>();
 		server.getWorlds().forEach(world -> {
 			Identifier dimid = world.getRegistryKey().getValue();
@@ -82,7 +86,7 @@ public class ModInit implements ModInitializer {
 		ModInit.dmThread.start();
 	}
 
-	public void onStop(MinecraftServer server) {
+	private void onStop(MinecraftServer server) {
 		apiServer.stop();
 		dm.stop();
 		try {
@@ -92,6 +96,24 @@ public class ModInit implements ModInitializer {
 		}
 	}
 
+	private void onJoin(ServerPlayNetworkHandler networkHandler, PacketSender sender, MinecraftServer server) {
+		DatabaseManager.getSingleton()
+				.queueOp(PlayerDAO.insert(
+						networkHandler.player.getUuid(),
+						networkHandler.player.getName().asString(),
+						java.time.Instant.now()
+				));
+	}
+
+	private void onQuit(ServerPlayNetworkHandler networkHandler, MinecraftServer server) {
+		DatabaseManager.getSingleton()
+				.queueOp(PlayerDAO.insert(
+						networkHandler.player.getUuid(),
+						networkHandler.player.getName().asString(),
+						java.time.Instant.now()
+				));
+	}
+
 	@Override
 	public void onInitialize() {
 		loadConfig(Paths.get(FabricLoader.getInstance().getConfigDir().toString(), "deltalogger.properties"));
@@ -99,6 +121,9 @@ public class ModInit implements ModInitializer {
 		ServerLifecycleEvents.SERVER_STARTING.register(this::onServerSetup);
 		ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStart);
 		ServerLifecycleEvents.SERVER_STOPPED.register(this::onStop);
+
+		ServerPlayConnectionEvents.JOIN.register(this::onJoin);
+		ServerPlayConnectionEvents.DISCONNECT.register(this::onQuit);
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> Commands.register(dispatcher));
 	}
