27f7f2fb873db3c7a5a55fccede1895b2f5b1d91
Gareth Coles <gdude2002@gmail.com>
2021-02-28 14:55:54 +0000

    Update deps, fix fireball crash (#29)
    * Update deps, fix fireball crash
    
    * Clean up imports

--- src/main/java/com/github/fabricservertools/deltalogger/mixins/events/FireballEntityMixin.java
+++ src/main/java/com/github/fabricservertools/deltalogger/mixins/events/FireballEntityMixin.java
@@ -1,20 +1,12 @@
 package com.github.fabricservertools.deltalogger.mixins.events;
 
-import com.github.fabricservertools.deltalogger.DatabaseManager;
-import com.github.fabricservertools.deltalogger.dao.EntityDAO;
-import com.github.fabricservertools.deltalogger.events.CreeperExplodeCallback;
-import net.minecraft.entity.Entity;
+import com.github.fabricservertools.deltalogger.events.FireballExplodeCallback;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.mob.GhastEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractFireballEntity;
 import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,21 +20,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // onCollision
 public class FireballEntityMixin extends AbstractFireballEntity {
-	public FireballEntityMixin(EntityType<? extends AbstractFireballEntity> entityType, LivingEntity livingEntity,
-							   double d, double e, double f, World world) {
-		super(entityType, livingEntity, d, e, f, world);
-	}
+    public FireballEntityMixin(EntityType<? extends AbstractFireballEntity> entityType, LivingEntity livingEntity,
+                               double d, double e, double f, World world) {
+        super(entityType, livingEntity, d, e, f, world);
+    }
 
-	@Inject(
-			method = "onCollision",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"),
-			cancellable = true
-	)
-	private void tryLogFireballExplosion(HitResult hit, CallbackInfo info) {
-		ActionResult result = CreeperExplodeCallback.EVENT.invoker().explode((CreeperEntity) (Object) this);
+    @Inject(
+            method = "onCollision",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"),
+            cancellable = true
+    )
+    private void tryLogFireballExplosion(HitResult hit, CallbackInfo info) {
+        ActionResult result = FireballExplodeCallback.EVENT.invoker().explode((FireballEntity) (Object) this, hit);
 
-		if (result != ActionResult.PASS) {
-			info.cancel();
-		}
-	}
+        if (result != ActionResult.PASS) {
+            info.cancel();
+        }
+    }
 }
