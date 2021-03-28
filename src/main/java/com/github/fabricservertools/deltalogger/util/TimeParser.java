22b9367fa725c8473b57065b2332b9a686fed9a1
yitzy299 <yitzy299@noreply.github.com>
2021-03-28 08:27:34 +0100

    Improve suggestions for rollback time values

--- src/main/java/com/github/fabricservertools/deltalogger/util/TimeParser.java
+++ src/main/java/com/github/fabricservertools/deltalogger/util/TimeParser.java
@@ -8,88 +8,85 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.server.command.ServerCommandSource;
 
 import java.time.Duration;
-import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 
 public class TimeParser {
-	public static TimeParserSuggestor INSTANCE = new TimeParserSuggestor();
+    public static TimeParserSuggestor INSTANCE = new TimeParserSuggestor();
 
-	private static class Pair<K, V> {
-		K key;
-		V value;
+    private static class Pair<K, V> {
+        K key;
+        V value;
 
-		public Pair(K key, V value) {
-			this.key = key;
-			this.value = value;
-		}
-	}
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
-	private static class TimeParserSuggestor implements SuggestionProvider<ServerCommandSource> {
-		private enum time_units {
-			S,//Seconds
-			M,//Minutes
-			H,//Hours
-			D,//Days
-			Y//Years
-		}
-		@Override
-		public CompletableFuture<Suggestions> getSuggestions(CommandContext context, SuggestionsBuilder builder) throws CommandSyntaxException {
-			String current = builder.getRemaining().toLowerCase();
-			for (time_units actions : time_units.values()) {
-				String name = actions.name().toLowerCase();
-				if (name.contains(current)) {
-					for(int i = 1; i < 11; i++) {
-						builder.suggest(i + name.toLowerCase(Locale.ROOT));
-					}
-				} else if (Character.isDigit(current.charAt(0))) {
-					builder.suggest(current.charAt(0) + name);
-				}
-			}
-			return builder.buildFuture();
-		}
-	}
+    private static class TimeParserSuggestor implements SuggestionProvider<ServerCommandSource> {
+        private enum time_units {
+            S,//Seconds
+            M,//Minutes
+            H,//Hours
+            D,//Days
+            Y//Years
+        }
 
-	private static Pair<Integer, String> removeAndReturn(String s, char c) throws NumberFormatException {
-		int index = s.indexOf(c);
-		if (index != -1) {
-			String s1 = s.substring(0, index);
-			s = s.substring(index + 1);
-			index = Integer.parseInt(s1);
-			return new Pair(index, s);
-		}
+        @Override
+        public CompletableFuture<Suggestions> getSuggestions(CommandContext context, SuggestionsBuilder builder) throws CommandSyntaxException {
+            String current = builder.getRemaining().toLowerCase();
+            for (time_units actions : time_units.values()) {
+                String name = actions.name().toLowerCase();
+                if (current.endsWith(name)) {
+                    return builder.buildFuture();
+                }
+                builder.suggest(current + name);
+            }
+            return builder.buildFuture();
+        }
+    }
 
-		return new Pair(0, s);
-	}
+    private static Pair<Integer, String> removeAndReturn(String s, char c) throws NumberFormatException {
+        int index = s.indexOf(c);
+        if (index != -1) {
+            String s1 = s.substring(0, index);
+            s = s.substring(index + 1);
+            index = Integer.parseInt(s1);
+            return new Pair(index, s);
+        }
 
-	public static Duration parseTime(String formattedText) throws NumberFormatException {
-		formattedText = formattedText.trim().replace(" ", "").toLowerCase();
+        return new Pair(0, s);
+    }
 
-		Duration duration = Duration.ZERO;
+    public static Duration parseTime(String formattedText) throws NumberFormatException {
+        formattedText = formattedText.trim().replace(" ", "").toLowerCase();
 
-		Pair<Integer, String> pair = removeAndReturn(formattedText, 'y');
-		duration = duration.plus(Duration.ofDays(pair.key * 365));
-		formattedText = pair.value;
+        Duration duration = Duration.ZERO;
 
-		pair = removeAndReturn(formattedText, 'd');
-		duration = duration.plus(Duration.ofDays(pair.key));
-		formattedText = pair.value;
+        Pair<Integer, String> pair = removeAndReturn(formattedText, 'y');
+        duration = duration.plus(Duration.ofDays(pair.key * 365));
+        formattedText = pair.value;
 
-		pair = removeAndReturn(formattedText, 'h');
-		duration = duration.plus(Duration.ofHours(pair.key));
-		formattedText = pair.value;
+        pair = removeAndReturn(formattedText, 'd');
+        duration = duration.plus(Duration.ofDays(pair.key));
+        formattedText = pair.value;
 
-		pair = removeAndReturn(formattedText, 'm');
-		duration = duration.plus(Duration.ofMinutes(pair.key));
-		formattedText = pair.value;
+        pair = removeAndReturn(formattedText, 'h');
+        duration = duration.plus(Duration.ofHours(pair.key));
+        formattedText = pair.value;
 
-		pair = removeAndReturn(formattedText, 's');
-		duration = duration.plus(Duration.ofSeconds(pair.key));
-		formattedText = pair.value;
+        pair = removeAndReturn(formattedText, 'm');
+        duration = duration.plus(Duration.ofMinutes(pair.key));
+        formattedText = pair.value;
 
-		if (!formattedText.isEmpty()) {
-			throw new IllegalStateException("Did not expect '" + formattedText + "'");
-		}
+        pair = removeAndReturn(formattedText, 's');
+        duration = duration.plus(Duration.ofSeconds(pair.key));
+        formattedText = pair.value;
 
-		return duration;
-	}
+        if (!formattedText.isEmpty()) {
+            throw new IllegalStateException("Did not expect '" + formattedText + "'");
+        }
+
+        return duration;
+    }
 }
